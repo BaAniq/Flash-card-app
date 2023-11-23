76839ce708efa93a2707b699f0c5d56bdc7fcd60
BaAniq <basia.anisimowicz@gmail.com>
2023-11-23 13:16:20 +0100

    Add a function that 'flips' card

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ card_words = {}
 french_words = pandas.read_csv("data/french_words.csv")
 french_english_dict = french_words.to_dict(orient='records')
 
+def correct_word_in_french(card_words):
+    global window_timer
+    window.after_cancel(window_timer)
+    canvas.itemconfig(card_image, image=photo_image_file_front)
+    card_word_french = card_words['French']
+    canvas.itemconfig(card_word, text=card_word_french)
+    canvas.itemconfig(card_language, text='French')
+    return card_word_french
+
 
 def next_card():
     global window_timer, card_words
@@ -16,6 +25,7 @@ def next_card():
     card_word_english = card_words['English']
     canvas.itemconfig(card_word, text=card_word_english)
     canvas.itemconfig(card_language, text='English')
+    window_timer = window.after(3000, correct_word_in_french, card_words)
 
 
 
@@ -36,7 +46,7 @@ card_word = canvas.create_text(400, 263, text='Word', font=('Ariel', 60, 'bold')
 canvas.grid(row=1, column=1, columnspan=2)
 
 wrong_button_image = PhotoImage(file='images/wrong.png',)
-wrong_button = Button(image=wrong_button_image, highlightthickness=0)
+wrong_button = Button(image=wrong_button_image, highlightthickness=0, command=next_card)
 wrong_button.grid(row=2, column=1)
 
 right_button_image = PhotoImage(file='images/right.png')
